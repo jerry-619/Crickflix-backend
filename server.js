51929fa35e50a7c9fecc5b701b1e16a3e92a0382
jerry-619 <nextm144@gmail.com>
2025-03-25 23:25:40 +0530

    up to date

--- server.js
+++ server.js
@@ -42,6 +42,7 @@ app.use(cors({
     process.env.FRONTEND_URL,
     process.env.ADMIN_URL,
     'http://localhost:5173',
+    'http://localhost:5174',
     'http://localhost:3000',
     'http://localhost:3001'
   ],

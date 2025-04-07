b0ec3ed63f28d5f18b23f610b88a5364a175c42b
jerry-619 <nextm144@gmail.com>
2025-04-07 17:22:09 +0530

    removed fantasy prediction

--- routes/predictionRoutes.js
+++ routes/predictionRoutes.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const {
-  getMatchFantasyXI,
   getMatchWinPrediction,
   getMatchTossPrediction
 } = require('../controllers/predictionController');
 
-router.get('/:matchId/fantasy', getMatchFantasyXI);
 router.get('/:matchId/match', getMatchWinPrediction);
 router.get('/:matchId/toss', getMatchTossPrediction);
 
